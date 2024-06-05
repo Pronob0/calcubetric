cdf6c9cbf5eea2c12ba8e5b7922e54688490dc3d
pronob <pronobsarker16@gmail.com>
2024-06-05 15:01:36 +0600

    hold push

--- project/resources/views/hold.blade.php
+++ project/resources/views/hold.blade.php
@@ -1,4 +1,3 @@
-
 <div class="d-flex mt-4 row">
     <div class="form-group col-md-6">
         <label for="stake">Side 1 Odds</label>
@@ -33,7 +32,7 @@
     </div>
 </div>
 
-{{-- alert  --}}
+{{-- alert --}}
 <div class="alert alert-danger d-none mt-3" id="halert">
     <strong>Provide Valid Odds</strong> <span id="hmsg"></span>
 </div>
@@ -48,42 +47,72 @@
 @push('js')
 
 <script>
-
     $(document).ready(function(){
         
 
 
         $('#side2').on('keyup', function(){
 
-            var side1 = parseFloat($("#side1").val());
-                var side2 = parseFloat($("#side2").val());
-                var bet_type = $("#hbet_type").val();
-                if(bet_type == 1){
+            var side1Odds = parseFloat($('#side1').val());
+            var side2Odds = parseFloat($('#side2').val());
+            var bet_type = $("#hbet_type").val();
+            if(bet_type == 1){
 
-                    // condition for american odds 
-                    if ((side1 > 0  && side1 < 99) ||  (side2 > 0 && side2 < 99)) {
-                        $("#halert").removeClass('d-none');
-                        return;
-                    }
-                    else{
-                        $("#halert").addClass('d-none');
-                    }
+            if ((side1Odds > 0  && side1Odds < 99) ||  (side2Odds > 0 && side2Odds < 99)) {
+                $("#halert").removeClass('d-none');
+                return;
+            }
+            else{
+                $("#halert").addClass('d-none');
+            }
 
-                var decimal1 = (side1 > 0) ? (side1 / 100) + 1 : (100 / Math.abs(side1)) + 1;
-                var decimal2 = (side2 > 0) ? (side2 / 100) + 1 : (100 / Math.abs(side2)) + 1;
-                var hold =  ((1 / decimal1) + (1 / decimal2)) -1;
-                console.log(hold);
-                var hold = hold * 100;
-                hold = hold.toFixed(2);
-                $("#hhold").text(hold + "%");
+            var totalRisk = 200;  // Assuming $100 on each side
+            var returnSide1 = (side1Odds / 100) * 100 + 100;
+            var returnSide2 = (side2Odds / 100) * 100 + 100;
 
-                }
-       
-            
-            
+        }
+        else if(bet_type == 2){
+            // decimal odds condition  
+            if ((side1Odds > 1  && side1Odds < 100) ||  (side2Odds > 1 && side2Odds < 100)) {
+                $("#halert").removeClass('d-none');
+                return;
+            }
+            else{
+                $("#halert").addClass('d-none');
+            }
 
-           
+        //    calculate hold for decimal odds
+
+            var totalRisk = 200;  // Assuming $100 on each side
+            var returnSide1 = side1Odds + 1;
+            var returnSide2 = side2Odds + 1;
+        }
+        
+
+            var averageTotalReturn = (returnSide1 + returnSide2) / 2;
+            var holdPercentage = ((totalRisk / averageTotalReturn) - 1) * 100;
+            $("#hhold").text(holdPercentage.toFixed(2) + "%");
+
+
         });
+
+
+
+        $('#reset').click(function(){
+            $('#side1').val('');
+            $('#side2').val('');
+            $('#hhold').text('');
+            $('#halert').addClass('d-none');
+        });
+        $('#hbet_type').change(function(){
+            $('#side1').val('');
+            $('#side2').val('');
+            $('#hhold').text('');
+            $('#halert').addClass('d-none');
+        });
+
+
+
 
         
     });
@@ -94,6 +123,6 @@
 
 
 </script>
-    
-    
+
+
 @endpush
